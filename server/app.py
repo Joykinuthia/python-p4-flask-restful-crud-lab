c1def07594f7515109f3a6a15e15e185bcab45e5
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-21 00:02:48 +0300

    flask restful crud lab

--- server/app.py
+++ server/app.py
@@ -17,8 +17,12 @@ db.init_app(app)
 api = Api(app)
 
 
-class Plants(Resource):
+@app.route('/')
+def index():
+    return "Welcome to the Plant API!", 200
 
+
+class Plants(Resource):
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -38,16 +42,38 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
-
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+        # Update all fields present in the request body
+        for field in ["name", "image", "price", "is_in_stock"]:
+            if field in data:
+                setattr(plant, field, data[field])
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
